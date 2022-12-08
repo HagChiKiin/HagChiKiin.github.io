996fd8b3b0c73ee1e8dcd3110d079433fe3d1a8e
HagChiKiin <emmangthai.dungkhaianhlabo@gmail.com>
2022-12-08 22:44:32 +0700

    ok

--- java/kiemtra/src/bai6.java
+++ java/kiemtra/src/bai6.java
@@ -10,8 +10,8 @@ public class bai6 {
         String number = sc.nextLine();
         String EMAIL_PATTERN =
                 "^[a-zA-Z][\\w-]+@([\\w]+\\.[\\w]+|[\\w]+\\.[\\w]{2,}\\.[\\w]{2,})$";
-        System.out.println(email + ": " + Pattern.matches(EMAIL_PATTERN, email));
+        System.out.println("email - "+email + ": " + Pattern.matches(EMAIL_PATTERN, email));
         String reg = "^(0|\\+84)(\\s|\\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\\d)(\\s|\\.)?(\\d{3})(\\s|\\.)?(\\d{3})$";
-        System.out.println(number + ": "+ Pattern.matches(reg , number));
+        System.out.println("sdt - "+number + ": "+ Pattern.matches(reg , number));
     }
 }
