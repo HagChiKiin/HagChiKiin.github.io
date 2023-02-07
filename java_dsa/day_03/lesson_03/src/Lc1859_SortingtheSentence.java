cf0b5cc71c4ab1f512cbb8b84294b8204715ffca
HagChiKiin <emmangthai.dungkhaianhlabo@gmail.com>
2023-02-07 16:04:34 +0700

    ok
    ok

--- java_dsa/day_03/lesson_03/src/Lc1859_SortingtheSentence.java
+++ java_dsa/day_03/lesson_03/src/Lc1859_SortingtheSentence.java
@@ -1,18 +1,16 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Lc1859_SortingtheSentence {
+
     public static String sortSentence(String s) {
-        String[] arr = s.split(" ");                // tao mảng ta có 4 phần tử
-        String[] newArr = new String[arr.length];         // tạo mảng mới có 4 phần tử
-
-        for (int i=0; i<arr.length; i++) {
-            int n = arr[i].length()-1;                      // khi i = 0 thì phần từ là is2 và có n = 2, i =1 n =8, i=2 n =4, i =3 n =1
-            int index = arr[i].charAt(n) - '0';             // index = 2  ( trừ đi gtri 0 ) ,=4, = 1, = 3
-            newArr[index-1] = arr[i].substring(0, n);       // newArr tại index = 1,3,0,2  sẽ bằng = ký tự từ  0 đến n-1(1,7,3,0)
+        String[] arr = s.split(" ");
+        Arrays.sort(arr, (o1, o2) -> o1.charAt(o1.length()-1)-o2.charAt(o2.length()-1));//sắp xếp theo vị trí cuối cùng các phần tử của mảng
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].substring(0,arr[i].length()-1);  // lấy ra phần tử từ index=0 > index = leng-2
         }
-        return String.join(" ", newArr);            // tạo chuỗi nối mảng newArr vs dấu cách và sắp xếp nó
+        return String.join(" ",arr);
     }
-
     public static void main(String[] args) {
         String s = "is2 sentence4 This1 a3";
         System.out.println(sortSentence(s));
--- java_dsa/day_03/lesson_03/src/Test.java
+++ java_dsa/day_03/lesson_03/src/Test.java
@@ -2,26 +2,36 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        Student[] students = { new Student(" A Thành Kiên",16 ,8),
+        Student[] students = { new Student("A Thành Kiên",16 ,8),
                 new Student("A Thành Kiên",18 ,9),
-                new Student("B Trần Anh",10 ,10),
-                new Student("C Phan Công",10 ,7)};
+                new Student("B Trần Anh",20 ,7),
+                new Student("C Phan Công",20 ,10)};
         System.out.println("1.Sắp xếp học sinh theo fullName, nếu giống fullName thì ai nhiều tuổi hơn thì đứng trước.");
-        Arrays.sort(students, (o1, o2) -> o1.fullName.compareTo(o2.fullName) + o2.age - o1.age);
+        Arrays.sort(students, (o1, o2) -> {
+            if(o1.fullName.equals(o2.fullName)){
+                return o2.age-o1.age;
+            }
+            return o1.fullName.compareTo(o2.fullName);
+        });
         for (Student p :students
         ) {
             System.out.println(p);
         }
         System.out.println("-----------------");
         System.out.println( "2.Sắp xếp học sinh theo tuổi tăng dần, nếu bằng tuổi thì xếp theo GPA giảm dần");
-        Arrays.sort(students, (o1, o2) -> o1.age-o2.age+o2.GPA-o1.GPA);
+        Arrays.sort(students, (o1, o2) -> {
+            if(o1.age==o2.age){
+                return o2.GPA-o1.GPA;
+            }
+            return o1.age-o2.age;
+        });
         for (Student p :students
         ) {
             System.out.println(p);
         }
-        System.out.println("3.Sắp xếp theo tên. (Ví dụ là Hoàng Văn A, thì chỉ xét A thôi)");
-        Arrays.sort(students, Comparator.comparing(o -> o.fullName.substring(o.fullName.lastIndexOf(" ") + 1)));
         System.out.println("-----------------");
+        System.out.println("3.Sắp xếp theo tên. (Ví dụ là Hoàng Văn A, thì chỉ xét A thôi)");
+        Arrays.sort(students, Comparator.comparing(o ->o.fullName.substring(o.fullName.lastIndexOf(" ")+1)));
         for (Student p :students
         ) {
             System.out.println(p);
