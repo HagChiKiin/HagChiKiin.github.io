6f06a1097cdfc7aa95755a4973ee5bd8a241c27d
HagChiKiin <emmangthai.dungkhaianhlabo@gmail.com>
2023-02-11 13:31:13 +0700

    2
    2

--- java_dsa/day_05/lesson_5/src/LC278.java
+++ java_dsa/day_05/lesson_5/src/LC278.java
@@ -4,7 +4,7 @@ public class LC278  {
         int right = n;
         while (left < right) {                 // Lặp cho đến khi phạm vi tìm kiem chi con 1
             int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) {  // kiểm tra bản xấu ở giữa
+            if (isBadVersion(mid)) {  // phương thức isbadversion tại mid
                 right = mid;       // nếu trả về true thi sẽ tiêp tuc kiểm tra ở mảng bên trái vs left vẫn 1 và right = mid
             } else {
                 left = mid + 1;     // nếu trả về false thì sẽ kiểm tra mảng ở bên phải vs left = mid +1 và right vẫn = n
@@ -12,7 +12,7 @@ public class LC278  {
         }
         return left;  // nêu left = right thì trả về left =1
     }
-    public static boolean  isBadVersion(int version){  
+    public static boolean  isBadVersion(int version){
         return version >=4;
     }
 
