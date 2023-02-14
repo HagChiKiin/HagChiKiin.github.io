41150b6f649a47c636caa1a3db330192a90062f4
HagChiKiin <emmangthai.dungkhaianhlabo@gmail.com>
2023-02-14 20:51:29 +0700

    sít ok
    sít ok

--- java_dsa/day_07/test/src/Lc1512.java
+++ java_dsa/day_07/test/src/Lc1512.java
@@ -2,7 +2,7 @@ public class Lc1512 {
     public int numIdenticalPairs(int[] nums) {
         int count = 0;   // tạo biến đếm
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j <nums.length ; j++) {   // đảm bảo các phần tử không bị trùng lặp
+            for (int j = i+1; j <nums.length ; j++) {   // j=i+1đảm bảo các phần tử không bị trùng lặp
                 if(nums[i] == nums[j]){  // num tại i = num tại j thì ta tăng biến đếm
                     count++;
                 }
