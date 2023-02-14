d20b8de56aa4ad0aa7b55fe27235366d2c31f629
HagChiKiin <emmangthai.dungkhaianhlabo@gmail.com>
2023-02-14 21:54:14 +0700

    forr trâu
    trau bò huc

--- java_dsa/day_07/test/src/Lc2418.java
+++ java_dsa/day_07/test/src/Lc2418.java
@@ -1,5 +1,17 @@
 public class Lc2418 {
-    public String[] sortNamesByHeights(String[] names, int[] heights) {
-
-
+    public String[] sortPeople(String[] names, int[] heights) {
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names.length - 1; j++) {   // for trâu  so sánh j vs j+1 nên j <= length-2
+                if (heights[j] < heights[j + 1]) {       // nếu phần tử chiều cao tại j mà nhỏ hơn tại j + 1 thì tiến hành đổi chỗ
+                    int t = heights[j];             // đổi chỗ chiều cao
+                    heights[j] = heights[j + 1];
+                    heights[j + 1] = t;
+                    String s = names[j];                // đổi chỗ tên
+                    names[j] = names[j + 1];
+                    names[j + 1] = s;
+                }
+            }
+        }
+        return names;  // trả về mảng tên theo yêu cầu đề bài
+    }
 }
