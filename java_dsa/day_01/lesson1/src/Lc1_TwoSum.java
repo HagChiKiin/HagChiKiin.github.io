69724ca4996f6d2e68dbf37fa0497ba00fb62f06
HagChiKiin <emmangthai.dungkhaianhlabo@gmail.com>
2023-02-26 19:03:07 +0700

    1 ok
    1 ok

--- java_dsa/day_01/lesson1/src/Lc1_TwoSum.java
+++ java_dsa/day_01/lesson1/src/Lc1_TwoSum.java
@@ -4,15 +4,15 @@ import java.util.Map;
 
 public class Lc1_TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        int[] result = new int[2];    // tạo mảng có 2 phần tử
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length ; i++) {
-           if(map.containsKey(target-nums[i])){
-               result[0] = map.get(target -nums[i]);
-               result[1] = i;
+           if(map.containsKey(target-nums[i])){    // nếu trong map có phần tử = target - num
+               result[0] = map.get(target -nums[i]);   // lay ra gia tri target - num gan vao rel 0
+               result[1] = i; // chi so i gan vao rel 1
                return result;
            }
-            map.put(nums[i], i);
+            map.put(nums[i], i);  // them vao map
         }
         return result;
     }
