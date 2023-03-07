1adbce61a7109b3b7e9a4f27804c1a188b87a891
HagChiKiin <emmangthai.dungkhaianhlabo@gmail.com>
2023-03-07 22:09:27 +0700

    ok
    ok

--- java_dsa/day_14/kiemtra/src/LC733.java
+++ java_dsa/day_14/kiemtra/src/LC733.java
@@ -1,19 +1,19 @@
 public class LC733 {
+
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int color = image[sr][sc];
-        if (color != newColor)
+        if (color != newColor)    // neu mau color chua phai mau moi thi goi ham de quy dfs de to mau cac pixcel lien ke
             dfs(image, sr, sc, color, newColor);
         return image;
     }
+    private boolean inNotValid(int[][] image, int r, int c, int color) {
+        return r < 0 || c<0 || r>=image.length || c>= image[0].length || image[r][c] !=color;
+    }
     public void dfs(int[][] image, int r, int c, int color, int newColor) {
-
-        if (r < 0 ) { return; }
-        if (c < 0) { return; }
-        if (r >= image.length) { return; }
-        if (c >= image[0].length) { return; }
-        if (image[r][c] != color) { return; }
+        if (inNotValid(image, r,c,color)) return;
 
         image[r][c] = newColor;
+        // lan luot lan toa 4 huong
         dfs(image, r-1, c, color, newColor);
         dfs(image, r, c-1, color, newColor);
         dfs(image, r+1, c, color, newColor);
